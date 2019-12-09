8eacbdcf50f666a8fcf6f9eff6dab9f741951359
jo030899 <alvarojohannes@gmail.com>
2019-12-10 00:28:16 +0700

    asd

--- Hansul/Proyek/Proyek/ProductDetail.aspx.designer.cs
+++ Hansul/Proyek/Proyek/ProductDetail.aspx.designer.cs
@@ -49,15 +49,6 @@ namespace Proyek {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-        /// <summary>
         /// LbGambar control.
         /// </summary>
         /// <remarks>
@@ -83,11 +74,7 @@ namespace Proyek {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlInputText qty;
-<<<<<<< HEAD
         
-=======
-
->>>>>>> be29eba6aefd857fa68727a9ec7b00656fa9bf49
         /// <summary>
         /// btn_insert control.
         /// </summary>
@@ -96,7 +83,6 @@ namespace Proyek {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btn_insert;
-<<<<<<< HEAD
         
         /// <summary>
         /// LBDescription control.
@@ -106,7 +92,5 @@ namespace Proyek {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label LBDescription;
-=======
->>>>>>> be29eba6aefd857fa68727a9ec7b00656fa9bf49
     }
 }
